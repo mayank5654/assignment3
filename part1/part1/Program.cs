60a22a150c459bfaeb494d34f30d04b5df2fad2d
mayank5654 <mayankkaushal99@gmail.com>
2023-11-08 21:30:35 +0530

    intial commit

---
+++ part1/part1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                // I. Using TryParse to convert a string to an integer
+                string intString = "42";
+                if (int.TryParse(intString, out int intValue))
+                {
+                    Console.WriteLine($"Using TryParse: The converted integer is {intValue}");
+                }
+                else
+                {
+                    Console.WriteLine("Using TryParse: Error converting to integer.");
+                }
+
+                // II. Using Convert to convert a string to a double
+                string doubleString = "3.14";
+                try
+                {
+                    double doubleValue = Convert.ToDouble(doubleString);
+                    Console.WriteLine($"Using Convert: The converted double is {doubleValue}");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Using Convert: Error converting to double.");
+                }
+
+                // III. Using Parse to convert a string to a decimal
+                string decimalString = "123.456";
+                try
+                {
+                    decimal decimalValue = decimal.Parse(decimalString);
+                    Console.WriteLine($"Using Parse: The converted decimal is {decimalValue}");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Using Parse: Error converting to decimal.");
+                }
+            
+        }
+
+    }
+}
+
---
+++ part2/part2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                // Task 1: Convert a string to an integer using TryParse
+                string intString = "42";
+                int result1;
+                if (int.TryParse(intString, out result1))
+                {
+                    int multipliedValue = result1 * 5;
+                    Console.WriteLine($"Task 1: The result is {multipliedValue}");
+                }
+                else
+                {
+                    Console.WriteLine("Task 1: Error converting to integer.");
+                }
+
+                // Task 2: Convert a string to a DateTime object using Convert
+                string dateString = "2023-10-31";
+                try
+                {
+                    DateTime result2 = Convert.ToDateTime(dateString);
+                    result2 = result2.AddMonths(-1);
+                    Console.WriteLine($"Task 2: The result is {result2}");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Task 2: Error converting to DateTime.");
+                }
+
+                // Task 3: Convert a string to a TimeSpan object using Parse
+                string timeSpanString = "02:30:00";
+                try
+                {
+                    TimeSpan result3 = TimeSpan.Parse(timeSpanString);
+                    result3 = result3.Add(new TimeSpan(2, 0, 0));
+                    Console.WriteLine($"Task 3: The result is {result3}");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Task 3: Error converting to TimeSpan.");
+                }
+            
+        }
+
+    }
+}
+
